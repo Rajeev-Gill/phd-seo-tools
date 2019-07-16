5cc34ceab045e9bb64464964f947885b12f582f4
Rajeev-Gill <rajeev.sgill@gmail.com>
2019-07-16 18:15:36 +0100

    initial commit, added all project files

---
+++ contentScript.js
@@ -0,0 +1,32 @@
+console.log("Programmatically injected contentScript");
+
+//Store page information in an object
+let pageInfo = {
+    currentPageURL: window.location.href,
+    links: {} //Pass objects into here
+}
+
+//Grab all links from html
+let links = document.getElementsByTagName("link");
+console.log(links);
+
+//For each link
+for(let i = 0; i < links.length; i++) {
+    //Add new a obj to pageInfo.links
+    pageInfo.links["link" + i] = {};
+
+    //Add properties to new object and store data
+    pageInfo.links["link" + i].target = links[i].baseURI;
+    pageInfo.links["link" + i].relationship = links[i].rel;
+}
+
+//Send a message containing the pageInfo properties 
+chrome.runtime.sendMessage({
+    //To add data to the message payload add another property below
+    url: pageInfo.currentPageURL, 
+    links: pageInfo.links
+}, response => {
+    console.log("Response", response);
+})
+
+console.log(pageInfo.links);
---
+++ popup.js
@@ -0,0 +1,90 @@
+console.log("This is popup.js talking...");
+
+//Popup functions
+let  popupFunction = {
+    sendMessage: () => {
+        //Query tabs for the active tab in the current window
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            //Execute a script on the active tab
+            chrome.tabs.executeScript(tabs[0].id, {file: 'contentScript.js'});
+          });
+        },
+        writeResourcehint: (rscHint) => {
+            //create a link element
+            let displayLink = document.createElement('a');
+            //set innerhtml of link to that of preloaded resource
+            //set attribute src of link to url of resource element.setAttribute('attribute', value)
+            //add link to DOM
+        }
+}
+
+
+//Variable to store recieved messages
+let  pageInfo = {
+    currentPageURL: '',
+    links: {},
+}
+
+
+//Variable to store popup UI components
+let  ui = {
+    popup: document.getElementById("main"),
+    displayUrlButton: document.getElementById("displayUrlButton"),
+    displayUrl: document.getElementById("displayUrl"),
+    displayLinksButton: document.getElementById("displayLinks"),
+    displayLinks: document.getElementById("displayLink")
+}
+
+//Listen for messages from contentScript using the following notation:
+//request.url
+//request.links
+chrome.runtime.onMessage.addListener(
+    (request, sender, sendResponse) => {
+        console.log(request)
+        pageInfo.currentPageURL = request.url;
+        pageInfo.links = request.links;
+        sendResponse({message: "info recieved by popup"});
+    }
+);
+
+//Display current page URL
+ui.displayUrlButton.addEventListener('click', () => {
+    //clear displayUrl div
+    ui.displayUrl.innerHTML = '';
+    //If there is a current URL
+    if(pageInfo.currentPageURL) {
+        //create a h2 element
+        let  urlText = document.createElement("h2");
+        //set the content of the h2 element
+        urlText.innerHTML = "Current page: " + pageInfo.currentPageURL;
+        //Add h2 element to page
+        ui.displayUrl.appendChild(urlText);
+    } else { console.log("Error") }
+})
+
+//Display links on page
+ui.displayLinksButton.addEventListener('click', () => {
+    //clear #displayLink div
+    ui.displayLinks = '';
+    //If we have links inside the pageInfo.links object
+    if(pageInfo.links) {
+    //Convert object to array
+    let links = Object.keys(pageInfo.links);
+    //Create another array to store complete linkinfo
+    let linksArray = []
+    //loop through initial array and deposit objects into complete linkinfo array
+    for (let i = 0; i < links.length; i++) {
+        linksArray.push(pageInfo.links[links[i]]);
+    }
+    //loop through linksArray and add write links to DOM
+    for (let i = 0; i < linksArray.length; i++) {
+        //write switch statement to evaluate for resource hints
+        //for each resource hint create a link element (writeResourcehint())
+    }
+    }
+})
+
+
+const arrayOfObj = Object.entries(pageInfo.links).map((e) => ({
+    [e[0]]: e[1]
+}));
