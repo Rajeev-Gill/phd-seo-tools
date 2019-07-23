275d96a1a6a9c8f79b27b4b314ef3ed6aff06a3d
Rajeev-Gill <rajeev.sgill@gmail.com>
2019-07-23 17:25:28 +0100

    fixed re-init bug

--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,35 @@
 console.log("Programmatically injected contentScript");
 
-//Clear pageInfo obj
-let pageInfo ='';
+if (typeof pageInfo != "object") {
+    //Store page information in an object
+    let pageInfo = {
+        currentPageURL: window.location.href,
+        links: {} //Pass objects into here
+    }
 
-//Store page information in an object
-pageInfo = {
-    currentPageURL: window.location.href,
-    links: {} //Pass objects into here
+    //Grab all links from html
+    let links = document.getElementsByTagName("link");
+    console.log(links);
+
+    //For each link
+    for (let i = 0; i < links.length; i++) {
+        //Add new a obj to pageInfo.links
+        pageInfo.links["link" + i] = {};
+
+        //Add properties to new object and store data
+        pageInfo.links["link" + i].target = links[i].href;
+        pageInfo.links["link" + i].relationship = links[i].rel;
+        pageInfo.links["link" + i].html = links[i].outerHTML;
+    }
+
+    //Send a message containing the pageInfo properties 
+    chrome.runtime.sendMessage({
+        //To add data to the message payload add another property below
+        url: pageInfo.currentPageURL,
+        links: pageInfo.links
+    }, response => {
+        console.log("Response", response);
+    })
+
+    console.log(pageInfo.links);
 }
-
-//Grab all links from html
-let links = document.getElementsByTagName("link");
-console.log(links);
-
-//For each link
-for(let i = 0; i < links.length; i++) {
-    //Add new a obj to pageInfo.links
-    pageInfo.links["link" + i] = {};
-
-    //Add properties to new object and store data
-    pageInfo.links["link" + i].target = links[i].href;
-    pageInfo.links["link" + i].relationship = links[i].rel;
-    pageInfo.links["link" + i].html = links[i].outerHTML;
-}
-
-//Send a message containing the pageInfo properties 
-chrome.runtime.sendMessage({
-    //To add data to the message payload add another property below
-    url: pageInfo.currentPageURL, 
-    links: pageInfo.links
-}, response => {
-    console.log("Response", response);
-})
-
-console.log(pageInfo.links);
